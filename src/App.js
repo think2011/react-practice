f85dabab08386bbe57e5cdedb15f7309ec171c78
think2011 <452125301@qq.com>
2015-11-11 22:52:01 +0800

    Updated 艰难的升级到react 0.14+

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+class App extends Component {
     render () {
         return (
             <h1>Hello, world.</h1>
         );
     }
 }
+
+export default App;
