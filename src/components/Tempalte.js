9eda88f294e780d36b373aa09f1a89e43c425b12
think2011 <452125301@qq.com>
2015-11-22 04:03:46 +0800

    init Redux-template

---
+++ src/components/Tempalte.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default class Tempalte extends Component {
+    render () {
+        return (
+            <section>
+
+            </section>
+        );
+    }
+}
