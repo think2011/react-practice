2875105d5bc4601cd478e616759cabf167ea6e0e
think2011 <452125301@qq.com>
2015-11-29 17:32:02 +0800

    Initialize Ajax-example

--- src/App.js
+++ src/App.js
@@ -3,10 +3,57 @@ import './index.css';
 import React, { Component } from 'react';
 
 class App extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            loading: null,
+            err    : null,
+            data   : null
+        };
+    }
+
+    handleLoad () {
+        let that = this;
+
+        that.setState({loading: true});
+
+        $.ajax({
+            url     : 'http://api.douban.com/v2/movie/top250',
+            dataType: 'jsonp'
+        }).then(function (rst) {
+            that.setState({
+                loading: false,
+                data   : rst
+            });
+        })
+    }
+
     render () {
-        return (
-            <h1 className="text-center">Hello, world.</h1>
-        );
+        let state = this.state;
+
+        if (state.err) {
+            return <span>出错了,请重试..</span>;
+        }
+
+        if (state.loading) {
+            return <span>正在加载中..</span>;
+        }
+
+        if (!state.loading) {
+            var list = state.data && state.data.subjects.map((v, k) => {
+                    return <div id={k} key={k}>{v.title}</div>;
+                });
+
+            return (
+                <section>
+                    <button onClick={this.handleLoad.bind(this)}>获取列表</button>
+
+                    <br/>
+                    {list}
+                </section>
+            );
+        }
     }
 }
 
