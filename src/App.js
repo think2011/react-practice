9b711aaa12e93283b86331880b7905fc810769b5
think2011 <452125301@qq.com>
2015-12-03 22:14:56 +0800

    Thinking in React ES6部分版

--- src/App.js
+++ src/App.js
@@ -1,11 +1,125 @@
 import './index.css';
-
 import React, { Component } from 'react';
 
-export default class App extends Component {
+class ProductCategoryRow extends Component {
     render () {
         return (
-            <h1 className="text-center">Hello, world.</h1>
+            <tr>
+                <th colSpan="2">{this.props.category}</th>
+            </tr>
+        );
+    }
+}
+
+class ProductRow extends Component {
+    render () {
+        var name = this.props.product.stocked
+            ? this.props.product.name
+            : (
+            <span style={{color:'red'}}>
+                {this.props.product.name}
+            </span>
+        );
+
+        return (
+            <tr>
+                <td>{name}</td>
+                <td>{this.props.product.price}</td>
+            </tr>
+        );
+    }
+}
+
+class ProductTable extends Component {
+    render () {
+        var rows         = [];
+        var lastCategory = null;
+
+        this.props.products.forEach(product => {
+            if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
+                return;
+            }
+
+            if (product.category !== lastCategory) {
+                rows.push(<ProductCategoryRow category={product.category} key={product.category}/>);
+            }
+
+            rows.push(<ProductRow product={product} key={product.name}/>);
+            lastCategory = product.category;
+        });
+
+        return (
+            <table>
+                <thead>
+                {rows.length === 0
+                    ? ''
+                    : (
+                    <tr>
+                        <td>Name</td>
+                        <td>Price</td>
+                    </tr>
+                )}
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
+        );
+    }
+}
+
+class SearchBar extends Component {
+    handleChange () {
+        this.props.onUserInput(
+            this.refs.filterTextInput.value,
+            this.refs.inStockOnlyInput.checked
+        );
+    }
+
+    render () {
+        return (<form>
+            <input type="search"
+                   value={this.props.filterText}
+                   ref="filterTextInput"
+                   onChange={this.handleChange.bind(this)}
+                   placeholder="Search..."/>
+            <p>
+                <label>
+                    <input type="checkbox"
+                           ref="inStockOnlyInput"
+                           onChange={this.handleChange.bind(this)}
+                           checked={this.props.inStockOnly}/>
+                    {' '}
+                    Only show products in stock
+                </label>
+            </p>
+        </form>);
+    }
+}
+
+export default class App extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            filterText : '',
+            inStockOnly: false
+        };
+    }
+
+    handleUserInput (filterText, inStockOnly) {
+        this.setState({filterText, inStockOnly});
+    }
+
+    render () {
+        return (
+            <div>
+                <SearchBar filterText={this.state.filterText}
+                           inStockOnly={this.state.inStockOnly}
+                           onUserInput={this.handleUserInput.bind(this)}/>
+
+                <ProductTable products={this.props.products}
+                              filterText={this.state.filterText}
+                              inStockOnly={this.state.inStockOnly}/>
+            </div>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 require("babel-polyfill");
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+
+var PRODUCTS = [
+    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+ReactDOM.render(<App products={PRODUCTS}/>, document.querySelector('#app'));
